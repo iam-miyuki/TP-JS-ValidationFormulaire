1906a6dfbd90f55eb99306c20a43eaf249a4ed7c
Miyuki <miyuki@mail.com>
2025-03-25 11:45:17 +0100

    modified

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
+"use strict";
+
 const loginField = document.querySelector("#login");
 // console.log(loginField);
 const pwdField = document.querySelector("#password");
 // console.log(pwdField);
 const checkBox = document.querySelector("#acceptPolicy");
 // console.log(checkBox);
-const myForm = document.querySelector("#loginForm");
-// console.log(myForm);
+const loginForm = document.querySelector("#loginForm");
+// console.log(loginForm);
 
-myForm.addEventListener("submit", function (e) {
+
+loginForm.addEventListener("submit", function (e) {
   e.preventDefault();
+
+  let isValid = true;
+
   if (loginField.value.length < 2 || loginField.value.length > 10) {
-    loginField.classList.add("is-invalid", "invaid-feedback");
-  } else {
-    console.log(loginField.value);
-  }
+    loginField.classList.add("is-invalid");
+    isValid = false;
+  } 
+  
   if (pwdField.value.length < 6 || pwdField.value.length > 20) {
-    pwdField.classList.add("is-invalid", "invaid-feedback");
-  } else {
-    console.log(pwdField.value);
-  }
+    pwdField.classList.add("is-invalid");
+    isValid = false;
+  } 
+
   if (!checkBox.checked) {
-    checkBox.classList.add("is-invalid", "invaid-feedback");
-  } else {
-    return;
+    checkBox.classList.add("is-invalid");
+    isValid = false;
+  } 
+
+  if (isValid) {
+    const inputs = loginForm.querySelectorAll("input")
+    for (let input of inputs){
+      input.classList.remove('is-invalid')
+    }
+    console.log(`login : ${loginField.value}`)
+    console.log(`password : ${pwdField.value}`)
   }
 });
